72b38c84998cdac9510fd17ed6cf62e2cc19e8b0
Anggra Aditra <aditra.anggra@gmail.com>
2022-05-08 10:16:52 +0700

    Add Webpack Config
    Fix dotenv issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react'
+import { db } from './firebase-config'
+import { collection, getDocs } from 'firebase/firestore'
 import './App.css'
 
-function App() {
-  return <div className='App'></div>
+const App = () => {
+  const [users, setUsers] = useState([])
+  const userCollectionRef = collection(db, 'users')
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(userCollectionRef)
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    }
+    getUsers()
+  }, [])
+
+  return (
+    <div className='App'>
+      {users.map((user) => (
+        <div key={user.id}>
+          <h1>Name: {user.name}</h1>
+          <h1>Age: {user.age}</h1>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default App
